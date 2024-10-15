e9e2a5d0284c46ed7352c7bb6f95d94c90ec3102
cocomongg <pjm4142@naver.com>
2024-10-16 01:05:23 +0900

    fix: WaitingQueueCommandTest 검증 수정

--- src/test/java/io/hhplus/concert/domain/waitingqueue/dto/WaitingQueueCommandTest.java
+++ src/test/java/io/hhplus/concert/domain/waitingqueue/dto/WaitingQueueCommandTest.java
@@ -1,8 +1,11 @@
 package io.hhplus.concert.domain.waitingqueue.dto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.hhplus.concert.domain.waitingqueue.dto.WaitingQueueCommand.CreateWaitingQueueCommand;
+import io.hhplus.concert.domain.waitingqueue.exception.WaitingQueueErrorCode;
+import io.hhplus.concert.domain.waitingqueue.exception.WaitingQueueException;
 import io.hhplus.concert.domain.waitingqueue.model.WaitingQueueStatus;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
@@ -22,21 +25,46 @@ class WaitingQueueCommandTest {
 
             // when, then
             assertThatThrownBy(() -> new CreateWaitingQueueCommand(invalidTokenValue,
-                WaitingQueueStatus.WAITED, LocalDateTime.now()));
+                WaitingQueueStatus.WAITED, LocalDateTime.now()))
+                .isInstanceOf(WaitingQueueException.class)
+                .hasMessage(WaitingQueueErrorCode.INVALID_CREATION_INPUT.getMessage());
         }
 
         @DisplayName("status가 유효하지 않은 값이면 WaitingQueueException이 발생한다.")
         @Test
         void should_ThrowWaitingQueueException_When_InvalidStatusValue() {
             assertThatThrownBy(() -> new CreateWaitingQueueCommand("token", null,
-                LocalDateTime.now()));
+                LocalDateTime.now()))
+                .isInstanceOf(WaitingQueueException.class)
+                .hasMessage(WaitingQueueErrorCode.INVALID_CREATION_INPUT.getMessage());
         }
 
         @DisplayName("expiredAt이 유효하지 않은 값이면 WaitingQueueException이 발생한다.")
         @Test
         void should_ThrowWaitingQueueException_When_InvalidExpiredAtValue() {
             assertThatThrownBy(() -> new CreateWaitingQueueCommand("token",
-                WaitingQueueStatus.WAITED, null));
+                WaitingQueueStatus.WAITED, null))
+                .isInstanceOf(WaitingQueueException.class)
+                .hasMessage(WaitingQueueErrorCode.INVALID_CREATION_INPUT.getMessage());
+        }
+
+        @DisplayName("모든 값이 유효하면 CreateWaitingQueueCommand 객체가 생성된다.")
+        @Test
+        void should_GenerateCreateWaitingQueueCommand_When_ValidValue() {
+            // given
+            String token = "token";
+            WaitingQueueStatus status = WaitingQueueStatus.WAITED;
+            LocalDateTime expiredAt = LocalDateTime.now();
+
+            // when
+            CreateWaitingQueueCommand command =
+                new CreateWaitingQueueCommand(token, status, expiredAt);
+
+            // then
+            assertThat(command).isNotNull();
+            assertThat(command.getToken()).isEqualTo(token);
+            assertThat(command.getStatus()).isEqualTo(status);
+            assertThat(command.getExpiredAt()).isEqualTo(expiredAt);
         }
     }
 }
