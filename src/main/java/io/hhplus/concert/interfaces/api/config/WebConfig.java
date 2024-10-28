d97c16c1e963f0b24509b1628b2f468177aa5576
cocomongg <pjm4142@naver.com>
2024-10-28 19:58:20 +0900

    fix: 활성화 토큰 검증 interceptor url pattern 수정

--- src/main/java/io/hhplus/concert/interfaces/api/config/WebConfig.java
+++ src/main/java/io/hhplus/concert/interfaces/api/config/WebConfig.java
@@ -15,8 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(waitingQueueTokenInterceptor)
-            .addPathPatterns("/api/v1/queues/tokens/order-info",
-                "/api/v1/concerts/*",
+            .addPathPatterns("/api/v1/concerts/*",
                 "/api/v1/members/*/points",
                 "/api/v1/payments/*");
     }
