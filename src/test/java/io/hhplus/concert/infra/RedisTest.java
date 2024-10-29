c9067b20ed0055c7c07722110df44b05c38e0e2f
cocomongg <pjm4142@naver.com>
2024-10-29 20:58:23 +0900

    feat: redis setting

---
+++ src/test/java/io/hhplus/concert/infra/RedisTest.java
@@ -0,0 +1,30 @@
+package io.hhplus.concert.infra;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void test() {
+        // given
+        String key = "testKey";
+        String value = "testValue";
+        redisTemplate.opsForValue().set(key, value);
+
+        // when
+        String result = String.valueOf(redisTemplate.opsForValue().get(key));
+
+        // then
+        assertThat(result).isEqualTo(value);
+    }
+
+}
