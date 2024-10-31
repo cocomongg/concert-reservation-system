6557eca432291fe023062491de50886dee7b0249
cocomongg <pjm4142@naver.com>
2024-10-31 17:26:42 +0900

    feat: redis 분산락 aop 추가

---
+++ src/main/java/io/hhplus/concert/domain/support/aop/DistributedLockAspect.java
@@ -0,0 +1,61 @@
+package io.hhplus.concert.domain.support.aop;
+
+import io.hhplus.concert.domain.support.utils.CustomSpringELParser;
+import io.hhplus.concert.domain.support.error.CoreErrorType;
+import io.hhplus.concert.domain.support.error.CoreException;
+import io.hhplus.concert.domain.support.lock.DistributedLockManager;
+import java.lang.reflect.Method;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Order(1)
+@Aspect
+@Component
+public class DistributedLockAspect {
+    private static final String DISTRIBUTE_LOCK_PREFIX = "LOCK:";
+
+    private final DistributedLockManager distributedLockManager;
+
+    @Around("@annotation(io.hhplus.concert.domain.support.aop.DistributedLock)")
+    public Object lock(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
+
+        String key = DISTRIBUTE_LOCK_PREFIX + CustomSpringELParser.getDynamicValue(
+            signature.getParameterNames(), joinPoint.getArgs(), distributedLock.key());
+        boolean locked = false;
+
+        try {
+            locked = distributedLockManager.tryLock(key, distributedLock.waitTime(),
+                distributedLock.leaseTime(), distributedLock.timeUnit());
+            if(!locked) {
+                return false;
+            }
+
+            return joinPoint.proceed();
+        } catch (Exception e) {
+            if(e instanceof CoreException) {
+                throw e;
+            } else {
+                throw new CoreException(CoreErrorType.INTERNAL_ERROR);
+            }
+        } finally {
+            try {
+                if(locked) {
+                    distributedLockManager.unlock(key);
+                }
+            } catch (Exception e) {
+                log.info("distributed lock already unlocked, key: {}", key);
+            }
+        }
+    }
+}
