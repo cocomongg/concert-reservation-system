7e50a66e323f1960e93259d553e1963d4082cb6c
cocomongg <pjm4142@naver.com>
2024-11-18 20:41:43 +0900

    feat: Enable 관련 애노테이션 분리

---
+++ src/main/java/io/hhplus/concert/config/SchedulerConfig.java
@@ -0,0 +1,25 @@
+package io.hhplus.concert.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@EnableScheduling
+@Configuration
+public class SchedulerConfig {
+
+    private static final String THREAD_NAME_PREFIX = "schedule-th-";
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler schedulerTask = new ThreadPoolTaskScheduler();
+
+        schedulerTask.setPoolSize(10);
+        schedulerTask.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        schedulerTask.initialize();
+
+        return schedulerTask;
+    }
+}
